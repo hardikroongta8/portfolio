ae969e0095338b33f405b20ac89b294b39f9038e
Hardik Roongta <hardikroongta8@gmail.com>
2024-07-09 21:53:24 +0530

    added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
 
 function App() {
-
   return (
-    <div>HELLO WORLD!</div>
+    <>
+    <div>Hi, I'm Hardik</div>
+    <div>
+    <a href="resume.pdf">Resume</a></div>
+    <div>
+    <a href='https://github.com/hardikroongta8'>Github</a>
+    </div>
+    </>
   );
 }
 
